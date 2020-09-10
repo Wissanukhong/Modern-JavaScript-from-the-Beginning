260f5ec28a6235f83b9de6b95316adfd239d75e9
Wissanu.khong <wissanu.khong@gmail.com>
2020-09-10 22:56:52 +0700

    feat: basic javaScript

---
+++ 1.introductoin/app.js
@@ -0,0 +1,9 @@
+// console
+console.clear();
+console.log(123);
+console.log("text");
+console.log([1, 2, 3, 4]);
+console.log({a:1, b:2});
+console.error('this is some error');
+console.warn('This is warning');
+console.time('hello');
