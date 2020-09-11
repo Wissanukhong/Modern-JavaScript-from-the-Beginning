6572a6e65547db6fbe4b6bb8b2a66a74db5644ba
Wissanu.khong <wissanu.khong@gmail.com>
2020-09-11 20:12:53 +0700

    feat: variables part 1

--- 1.introductoin/app.js
+++ 1.introductoin/app.js
@@ -7,3 +7,15 @@ console.log({a:1, b:2});
 console.error('this is some error');
 console.warn('This is warning');
 console.time('hello');
+
+// variables
+var name = 'John doe';
+console.log(name);
+
+// new assignment 1
+name = 'Steve smith';
+console.log(name);
+
+// new assignment 2
+name = 'Joe learn';
+console.log(name);
