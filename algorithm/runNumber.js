30eae06cd4bca40f172b4d181ee9d69cfc548ab4
Wissanu.khong <wissanu.khong@gmail.com>
2020-09-21 14:01:42 +0700

    debug: algorithm

--- algorithm/runNumber.js
+++ algorithm/runNumber.js
@@ -7,9 +7,7 @@ for (i = 0; i < 128; i++) {
 
 // Problem 2+4+6+8...248
 let sum = 0;
-for (i = 2; i < 248;) {
-
+for (i = 2; i < 248; i += 2) {
     sum = sum + i;
-    i += 2;
     console.log(sum);
 }
