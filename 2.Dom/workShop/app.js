406714d712f3dc9901804c29dd8960c5d2d5ab1f
Wissanu.khong <wissanu.khong@gmail.com>
2020-09-22 16:09:12 +0700

    refactoring code workshop

--- 2.Dom/workShop/app.js
+++ 2.Dom/workShop/app.js
@@ -3,10 +3,11 @@
 const liOdd = document.querySelectorAll('li:nth-child(odd)');
 const liEven = document.querySelectorAll('li:nth-child(even)');
 
-liOdd.forEach(function(li) {
+liOdd.forEach(function (li) {
     li.style.background = '#ccc';
 });
 
-liEven.forEach(function(li) {
+liEven.forEach(function (li) {
     li.style.background = '#f4f4f4';
 });
+
