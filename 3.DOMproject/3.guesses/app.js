ec8ddaab622e2a29564b3ef914ccc148883a333c
Wissanu.khong <wissanu.khong@gmail.com>
2020-11-04 17:30:59 +0700

    feat : guess function

--- 3.DOMproject/3.guesses/app.js
+++ 3.DOMproject/3.guesses/app.js
@@ -7,49 +7,23 @@ GAME FUNCTION:
 - Let player choose to play again
 */
 
-// Game values
-let min = 1,
-    max = 10,
-    winningNum = 2,
-    guessesLeft = 3;
+// game value
+let min = 0;
+let max = 10;
 
-// UI Elements
-const game = document.querySelector('#game'),
-      minNum = document.querySelector('.min-num'),
-      maxNum = document.querySelector('.max-num'),
-      guessBtn = document.querySelector('#guess-btn'),
-      guessInput = document.querySelector('#guess-input'),
-      message = document.querySelector('.message'); 
+// game UI
+const game = document.querySelector('#game');
+const minNum = document.querySelector('.min-num');
+const maxNum = document.querySelector('.max-num');
+const guessBtn = document.querySelector('#guess-btn');
+const guessInput = document.querySelector('#guess-input');
+const message = document.querySelector('.message');
 
 // Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
-      
+
 // Listen for guess
-guessBtn.addEventListener('click', function(){
-  let guess = parseInt(guessInput.value);
-  
-  // Validate
-  if(isNaN(guess) || guess < min || guess > max){
-    setMessage(`Please enter a number between ${min} and ${max}`, 'red');
-  }
-
-  // Check if won
-  if(guess === winningNum){
-    // Disable input
-    guessInput.disabled = true;
-    // Change border color
-    guessInput.style.borderColor = 'green';
-    // Set message
-    setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
-
-  } else {
-
-  }
+guessBtn.addEventListener('click', function() {
+    console.log(guessInput.value);
 });
-
-// Set message
-function setMessage(msg, color){
-  message.style.color = color;
-  message.textContent = msg;
-}
