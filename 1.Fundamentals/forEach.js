106f4269c2d74fedc43cd1698fa33d7c0f70c2c1
Wissanu.khong <wissanu.khong@gmail.com>
2020-11-23 15:57:47 +0700

    Test : getElementById

---
+++ 1.Fundamentals/forEach.js
@@ -0,0 +1,10 @@
+// forEach เป็นการ Loop array เพื่อนำค่าออกมาแสดง
+let cars = ['Toyota', 'Honda', 'Mazda'];
+console.log(cars); //[ 'Toyota', 'Honda', 'Mazda' ]
+
+cars.forEach(function (car) {
+    console.log(car);
+    // Toyota
+    // Honda
+    // Mazda
+});
---
+++ test/changeStyle/app.js
@@ -0,0 +1,1 @@
+document.getElementById('bg').style.background = 'red';
