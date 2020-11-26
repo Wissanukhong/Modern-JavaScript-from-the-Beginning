67c08a078963bbfa1dfec10176720e16fec38421
Wissanu.khong <wissanu.khong@gmail.com>
2020-11-26 15:22:41 +0700

    feat: OOP constuctors

--- 4.OOP/constructors/app.js
+++ 4.OOP/constructors/app.js
@@ -22,3 +22,18 @@ function Person(name, dob) {
 const Nov = new Person('Wissanu', '10-05-1992');
 
 console.log(Nov.calculateAge());
+
+// String
+const name1 = 'Wissanu';
+const name2 = new String('Wissanu');
+console.log(typeof name2);
+
+// Number
+const number1 = 5;
+const number2 = new Number(5);
+console.log(typeof number2);
+
+// Boolean
+const bool1 = true;
+const bool2 = new Boolean(true);
+console.log(`ค่านี้คือ` + ' ' + typeof bool2);
