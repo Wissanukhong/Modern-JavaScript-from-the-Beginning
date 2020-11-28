0272c02ad03f1390098394a0902bbc84b1da5a87
Wissanu.khong <wissanu.khong@gmail.com>
2020-11-28 10:44:53 +0700

    reafctor : OOP class ES6

--- 4.OOP/05ClassES6/app.js
+++ 4.OOP/05ClassES6/app.js
@@ -1,22 +1,37 @@
-// javaScript class
+// class Person
 class Person {
-  constructor(firstname, lastName, dob) {
-    this.firstName = firstname;
+  constructor(firstName, lastName, dob) {
+    this.firstName = firstName;
     this.lastName = lastName;
     this.birthday = new Date(dob);
   }
 
-  hello() {
-    return `Hello my name is ${this.firstName} ${this.lastName}`;
+  greeting() {
+    return `Hello there my name is ${this.firstName} ${this.lastName}`;
   }
 
-  calculateAge() {
+  calculate() {
     const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
+    const age = new Date(diff);
+    return Math.abs(age.getUTCFullYear() - 1970);
   }
 }
 
-const Wissanu = new Person('Wissanu', 'Khongjandee', '1993');
+const Wissanu = new Person('Wissanu', 'khongjandee', '10-05-1993');
+const John = new Person('John', 'Doe', '1-10-1990');
 
-console.log(`${Wissanu.hello()} I am  ${Wissanu.calculateAge()} years old`);
+console.log(`${Wissanu.greeting()} I am ${Wissanu.calculate()} years old`);
+console.log(`${John.greeting()} I am ${John.calculate()} years old`);
+
+// class Cars
+class Cars {
+  conststuctor(brand, type, owner) {
+    this.brand = brand;
+    this.type = type;
+    this.owner = owner;
+  }
+}
+
+const Toyota = new Cars('Toyota', 'Revo', 'Wissanu');
+
+console.log(Toyota);
