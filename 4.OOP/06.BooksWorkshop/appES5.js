bc1b836978a4aa11e46cbfa06f0ce214a81ca4de
Wissanu.khong <wissanu.khong@gmail.com>
2020-12-04 09:37:03 +0700

    OOP Add book workshop function add book

---
+++ 4.OOP/06.BooksWorkshop/appES5.js
@@ -0,0 +1,46 @@
+// Book constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI Constructor
+function UI() {}
+
+// Add Book to List
+UI.prototype.addBookToList = function (book) {
+  const list = document.getElementById('book-list');
+
+  // Create tr element
+  const row = document.createElement('tr');
+
+  // Insert cols
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X<a></td>
+  
+  `;
+
+  list.appendChild(row);
+};
+
+// Event Listener
+document.getElementById('book-form').addEventListener('submit', function (e) {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const isbn = document.getElementById('isbn').value;
+
+  // Instantiate book
+  const book = new Book(title, author, isbn);
+
+  // Instantiate UI
+  const ui = new UI();
+
+  // Add book to list
+  ui.addBookToList(book);
+
+  e.preventDefault();
+});
